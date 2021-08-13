f5bb75bc37d2681011ae378d446f370d8b494f08
Kerem Ayan <keremayan002@gmail.com>
2021-08-13 14:44:48 +0300

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http' 
+import {HttpClientModule} from '@angular/common/http';
+import { BrandComponent } from './component/brand/brand.component';
+import { ColorComponent } from './component/color/color.component' 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/brand.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BrandResponseModel } from '../models/brandResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrandService {
+ apiUrl:string="https://localhost:44340/api/brands/getall"
+  constructor(private httpClient:HttpClient) { }
+  getBrands():Observable<BrandResponseModel>{
+    return this.httpClient.get<BrandResponseModel>(this.apiUrl);
+  }
+}
