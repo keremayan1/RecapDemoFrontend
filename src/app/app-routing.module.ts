be7780e93ad105042ad74a7edfd6cf52db234c6f
Kerem Ayan <keremayan001@hotmail.com>
2021-09-13 19:22:51 +0300

    added cardetail tools

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarDetailComponent } from './component/car-detail/car-detail.component';
 import { CarComponent } from './component/car/car.component';
 
 const routes: Routes = [
-  {path:"",component:CarComponent},
-  {path:"cars",component:CarComponent},
-  {path:"cars/brand/:brandId",component:CarComponent},
-  {path:"cars/color/:colorId",component:CarComponent} 
+  {path:"",component:CarDetailComponent},
+  {path:"cardetails",component:CarDetailComponent},
+  {path:"cardetails/brand/:brandId",component:CarDetailComponent},
+  {path:"cardetails/color/:colorId",component:CarDetailComponent},
+  {path:"cardetails/brand/:brandId/color/:colorId",component:CarDetailComponent} 
 ];
 
 @NgModule({
--- src/app/component/car-detail/car-detail.component.ts
+++ src/app/component/car-detail/car-detail.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Brand } from 'src/app/models/brand';
+import { CarDetail } from 'src/app/models/cardetail';
+import { Color } from 'src/app/models/color';
+import { BrandService } from 'src/app/services/brand.service';
+import { CarDetailService } from 'src/app/services/car-detail.service';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-car-detail',
   templateUrl: './car-detail.component.html',
-  styleUrls: ['./car-detail.component.css']
+  styleUrls: ['./car-detail.component.css'],
 })
 export class CarDetailComponent implements OnInit {
-
-  constructor() { }
+  carDetails: CarDetail[] = [];
+  brands: Brand[] = [];
+  colors: Color[] = [];
+  constructor(
+    private carDetailService: CarDetailService,
+    private brandService: BrandService,
+    private colorService: ColorService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.getCarBrands();
+    this.getCarColors();
 
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['brandId'] && params['colorId']) {
+        this.getCarDetailByBrandIdAndColorId(
+          params['brandId'],
+          params['colorId']
+        );
+      } else if (params['brandId']) {
+        this.getCarDetailByBrandId(params['brandId']);
+      } else if (params['colorId']) {
+        this.getCarDetailByColorId(params['colorId']);
+      } else {
+        this.getCarDetails();
+      }
+    });
+  }
+  getCarDetails() {
+    this.carDetailService
+      .getCarDetail()
+      .subscribe((response) => (this.carDetails = response.data));
+  }
+  getCarDetailByBrandId(brandId: number) {
+    this.carDetailService
+      .getCarDetailByBrandId(brandId)
+      .subscribe((response) => (this.carDetails = response.data));
+  }
+  getCarDetailByColorId(colorId: number) {
+    this.carDetailService
+      .getCarDetailByColorId(colorId)
+      .subscribe((response) => (this.carDetails = response.data));
+  }
+  getCarDetailByBrandIdAndColorId(brandId: number, colorId: number) {
+    this.carDetailService
+      .getCarDetailByBrandIdAndColorId(brandId, colorId)
+      .subscribe((response) => (this.carDetails = response.data));
+  }
+  getCarDetailByMinPriceAndMaxPrice(minPrice: number, maxPrice: number) {
+    this.carDetailService
+      .getCarDetailByMinPriceAndMaxPrice(minPrice, maxPrice)
+      .subscribe((response) => (this.carDetails = response.data));
+  }
+  getCarBrands() {
+    this.brandService
+      .getBrands()
+      .subscribe((response) => (this.brands = response.data));
+  }
+  getCarColors() {
+    this.colorService
+      .getColors()
+      .subscribe((response) => (this.colors = response.data));
+  }
 }
